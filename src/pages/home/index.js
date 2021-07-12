2d96502a2430463c7285b54fb30016697b485917
Rodolfo Pessanha <j.rodolfopps@gmail.com>
2021-07-12 09:12:40 -0300

    First test of window name

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,17 @@
+import {useEffect} from 'react'
 import Teste from "components/teste"
 
-const Home = () => <Teste />
+const Home = () => {
+  useEffect(() => {
+    if (!window.name) {
+      alert("Não tem Name")
+      window.name = 'teste';
+    } else {
+      alert(`O nome é ${window.name}`);
+    }
+  }, [])
+
+ return <Teste />
+}
 
 export default Home
